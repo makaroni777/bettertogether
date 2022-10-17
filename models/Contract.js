503e8c785682281d3ef658b5169819bbc57439ee
M_Sta <99408062+makaroni777@users.noreply.github.com>
2022-10-17 15:51:40 +0200

    Add files via upload

---
+++ models/Contract.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const contractSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true,
+
+    },
+    number: {
+        type: String,
+        required: true,
+
+    },
+    product: {
+        type: String,
+        required: true,
+
+
+    },
+    gtc: {
+        type: String,
+        required: true,
+
+    }
+});
+
+
+const Contract = mongoose.model('contract', contractSchema);
+
+module.exports = Contract;
